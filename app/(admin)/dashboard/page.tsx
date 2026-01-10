2e9d7c9fe814c92e362058fa550005517f42b4ac
shuneo <solshuneo@gmail.com>
2026-01-10 17:53:49 +0700

    init

---
+++ app/(admin)/dashboard/page.tsx
@@ -0,0 +1,8 @@
+export default function DashboardPage() {
+    return (
+        <div>
+            <h1>Admin Dashboard</h1>
+            <p>Welcome to the admin dashboard.</p>
+        </div>
+    );
+}
